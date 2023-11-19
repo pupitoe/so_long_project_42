c363291769d4c28517edbf7266a54a1a5c1932b7
Tom Lasseret <tlassere@k1r1p4.42mulhouse.fr>
2023-11-19 01:09:18 +0100

    finish fill flood

--- sources/ft_check_map_flood_fill.c
+++ sources/ft_check_map_flood_fill.c
@@ -35,16 +35,22 @@ static t_pos	ft_get_spawn(char **map)
 			spawn.x = car - map[spawn.y];
 		spawn.y += 1;
 	}
+	spawn.y -= 1;
 	ft_printf("spawn, x %d; y : %d\n", spawn.x, spawn.y);
 	return (spawn);
 }
 
-static	void ft_flood_fill_rec(t_item_count *item, char **map, t_pos vector)
+static void	ft_flood_fill_rec(t_item_count *item, char **map, t_pos vector)
 {
 	if (ft_strchr("12M", map[vector.y][vector.x]))
 		return ;
 	ft_put_element(item, map[vector.y][vector.x]);
-	ft_flood_fill(item, map, );
+	map[vector.y][vector.x] = '2';
+	ft_printf("%w\n", map);
+	ft_flood_fill_rec(item, map, (t_pos){vector.x + 1, vector.y, 0});
+	ft_flood_fill_rec(item, map, (t_pos){vector.x - 1, vector.y, 0});
+	ft_flood_fill_rec(item, map, (t_pos){vector.x, vector.y + 1, 0});
+	ft_flood_fill_rec(item, map, (t_pos){vector.x, vector.y - 1, 0});
 }
 
 int	ft_flood_fill(char **map, t_item_count *items)
@@ -59,8 +65,9 @@ int	ft_flood_fill(char **map, t_item_count *items)
 	buffer_item = ft_make_item_count();
 	if (buffer_item == NULL)
 		return (ft_free_tab(buffer_map), -1);
+	ft_flood_fill_rec(buffer_item, buffer_map, ft_get_spawn(buffer_map));
+	ft_printf("new_tab :\n%w", buffer_map);
 	tmp = ft_cmp_item(items, buffer_item);
-
 	free(buffer_item);
 	ft_free_tab(buffer_map);
 	return (tmp);
